ab83fda232129a24aebb813c27e29903882a9258
sadikatgi-rgb <sadikatgi@gmail.com>
2026-02-09 17:08:53 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const cacheName = 'viva-directory-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
